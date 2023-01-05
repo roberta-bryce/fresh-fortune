1631bc4f7f8d54ae8798d13acb95a2892ef175fa
Roberta <111713483+roberta-bryce@users.noreply.github.com>
2023-01-05 17:38:58 +1300

    addFortune db function and test

--- server/db/dbFortune.js
+++ server/db/dbFortune.js
@@ -4,6 +4,10 @@ function getFortunes(db = connection) {
   return db('fortune').select()
 }
 
+function addFortune(newFortune, db = connection) {
+  return db('fortune').insert(newFortune)
+}
 module.exports = {
   getFortunes,
+  addFortune,
 }
--- server/db/tests/dbFortune.test.js
+++ server/db/tests/dbFortune.test.js
@@ -1,4 +1,4 @@
-const { getFortunes } = require('../dbFortune')
+const { getFortunes, addFortune } = require('../dbFortune')
 
 const knex = require('knex')
 const config = require('../knexfile').test
@@ -14,5 +14,18 @@ describe('getFortunes', () => {
       expect(fortune).toHaveLength(3)
       expect(fortune[2].fortune).toBe('You have some news coming')
     })
+  })
+})
+
+describe('addFortune', () => {
+  test('adds a new fortune to the table', () => {
+    return addFortune({ fortune: 'your test will work' }, testDb)
+      .then(() => {
+        return testDb('fortune').select()
+      })
+      .then((fortune) => {
+        expect(fortune).toHaveLength(4)
+        expect(fortune[3].fortune).toBe('your test will work')
+      })
   })
 })
