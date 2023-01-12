5d21ad66cf2f27c0aa79f7d173a4b1da647b5871
Roberta <111713483+roberta-bryce@users.noreply.github.com>
2023-01-12 18:17:09 +1300

    app and index set up with routes home page created

--- client/index.js
+++ client/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import store from './store'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>,
     document.getElementById('app')
   )
 })
