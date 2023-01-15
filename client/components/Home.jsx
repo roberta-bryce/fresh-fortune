af7f219550cf121562247e78b35419cba26b20af
Roberta <111713483+roberta-bryce@users.noreply.github.com>
2023-01-16 12:11:01 +1300

    home component setup

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function Home() {
-  return <h1> Why Hello There</h1>
+  return (
+    <>
+      <div>
+        <h1>Fresh Fortune</h1>
+        <h3>Click the Cookie to See Your Fortune</h3>
+        <h3>Edit</h3>
+      </div>
+      <div></div>
+    </>
+  )
 }
 
 export default Home
