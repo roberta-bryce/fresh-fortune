e656ffb5fa5c52aa155741b51392512f395e9682
Roberta <111713483+roberta-bryce@users.noreply.github.com>
2023-01-31 21:19:55 +1300

    css setup heading and bg on home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,14 +6,13 @@ function Home() {
   return (
     <>
       <div>
-        <h1>Fresh Fortune</h1>
-        <h3>Click the Cookie to See Your Fortune</h3>
+        <h1 className="freshHeading">Fresh Fortune</h1>
+        <div>
+          <Cookie />
+        </div>
         <button>
           <Link to="/edit">Edit</Link>
         </button>
-      </div>
-      <div>
-        <Cookie />
       </div>
     </>
   )
